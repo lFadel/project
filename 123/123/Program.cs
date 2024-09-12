73cbcd4761b11249bd881cdf96a027b6f7a9c3a1
Fade <galimov-stepan@bk.ru>
2024-09-12 11:21:05 +0400

    калькулятор 2.0
    добавлены возможности деления и умножения.

---
+++ 123/123/Program.cs
@@ -0,0 +1,24 @@
+﻿double a = double.Parse(Console.ReadLine());
+var c = Console.ReadLine();
+double b = double.Parse(Console.ReadLine());
+if (c == "+")
+{
+    Console.WriteLine(a+b);
+}
+else if (c == "-")
+{
+    Console.WriteLine(a-b);
+}
+else if(c == "/")
+{
+    Console.WriteLine(a/b);
+}
+else if (c == "*")
+{
+    Console.WriteLine(a*b);
+}
+else
+{
+    Console.WriteLine("Недопустимое значение");
+}
+
