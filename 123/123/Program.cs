0d7dd9ed671df258c7f794b56c51adf0573fce36
Fade <galimov-stepan@bk.ru>
2024-09-12 11:11:24 +0400

    Add files via upload

---
+++ 123/123/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+var c = Console.ReadLine();
+if (c == "+")
+{
+    Console.WriteLine(a+b);
+}
+else if (c == "-")
+{
+    Console.WriteLine(a-b);
+}
+else
+{
+    Console.WriteLine("Недопустимое значение");
+}
+
